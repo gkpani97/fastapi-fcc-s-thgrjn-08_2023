e26c2316b20a1d160361122e9c31bc4a9c47dc93
Gurlop Kishore Pani <gkpani97@gmail.com>
2023-08-12 10:11:18 +0530

    tried pydantic classes

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/post")
-async def root(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"message": "Received Payload"}
+async def root(post: Post):
+    print(post.model_dump())
+    return {"data": post}
