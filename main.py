bf7e7a1ebe551460140be03eedd31a2da7fc36af
Gurlop Kishore Pani <gkpani97@gmail.com>
2023-08-12 09:28:07 +0530

    added the post method

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
-app.get("/")
+@app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/post")
+async def root(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"message": "Received Payload"}
